5b429096a32fef4b61500a80e0d381e5174e6377
kambehba <kamparsen@yahoo.com>
2017-01-10 10:05:54 -0500

    fixed the Rx casing error. changed rxjs/RX rxjs/Rx

--- TaskTrackerClient/src/app/services/task.service.ts
+++ TaskTrackerClient/src/app/services/task.service.ts
@@ -2,28 +2,22 @@ import { Injectable,EventEmitter } from '@angular/core';
 import {Http,Response,Headers,RequestOptions} from '@angular/http';
 import {Task} from '../models/taskModel';
 import {Observable} from "rxjs";
-import 'rxjs/RX';
+import 'rxjs/Rx';
 
 @Injectable()
 export class TaskService {
 
-  private tasks:Task[] = [new Task('dafsd','sdfsdf','sdfsdf'),new Task('daf5555sd','sdfsd5555f','5555')];
+  private tasks:Task[]; 
   private task:Task;
   private selectedTask:Task;
   taskSelectedEvent = new EventEmitter<Task>();
 
   constructor(private http:Http) {this.task = new Task('','','') }
 
-  //  getTask(){
-    
-  //     return this.http.get('http://localhost:3000/api/tasks').map((response:Response)=>response.json());
-  //  }
-
-
-    getTask() {
+  
+   getTask() {
         return this.http.get('http://localhost:3000/api/tasks')
             .map((response: Response) => {
-                //const tasks = response.json().obj;
                 const tasks = response.json();
                 let transformedTasks: Task[] = [];
                 for (let task of tasks) {
@@ -31,48 +25,43 @@ export class TaskService {
                 }
                 this.tasks = transformedTasks;
                 return transformedTasks;
-                //return tasks;
+                
             })
-            //.catch((error: Response) => Observable.throw(error.json()));
              .catch(this.handleError);
-    }
+   }
 
 
    addTask(task:Task){
 
      const body = JSON.stringify(task);
-    
      const headers = new Headers({'Content-Type':'application/json', 'Accept': 'application/json'});
      return this.http.post('http://localhost:3000/api/tasks',body,{headers:headers})
      .map((response:Response)=>response.json())
      .catch(this.handleError);
-     //.catch((error:Response)=>Observable.throw(this.handleError));
+     
    }
 
     editTask(task:Task){
-
      const body = JSON.stringify(task);
     
      const headers = new Headers({'Content-Type':'application/json', 'Accept': 'application/json'});
      return this.http.post('http://localhost:3000/api/tasks',body,{headers:headers})
      .map((response:Response)=>response.json())
      .catch(this.handleError);
-     //.catch((error:Response)=>Observable.throw(this.handleError));
+     
    }
 
    deleteTask(task:Task)
    {
-     //console.log(task.description);
      
      this.task = task;
-     
      const body = JSON.stringify(task);
       console.log(body);
      const headers = new Headers({'Content-Type':'application/json', 'Accept': 'application/json'});
      return this.http.delete('http://localhost:3000/api/tasks',new RequestOptions({headers:headers,body:body}))
      .map((response:Response)=>response.json())
      .catch(this.handleError);
-     //.catch((error:Response)=>Observable.throw(this.handleError));
+     
    }
 
     taskSelectedForEdit(task: Task) {
@@ -86,11 +75,6 @@ export class TaskService {
     getSelectedTask(){
       return  this.selectedTask;
     }
-
-
-
-
-
 
 
    private handleError(error:any){
